2f3e5f1c71b08ab52510ef1daabc2c28042ee034
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-04-05 18:51:43 +0300

    day 5 commit

--- day_05.cpp
+++ day_05.cpp
@@ -8,15 +8,33 @@ using std::vector;
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
+        int i = 0;
+        int j = 0;
+        int profit = 0;
+        int size = prices.size();
 
+        if (size > 0) {
+            while (i < size - 1) {
+                if (prices[i] < prices[i + 1]) {
+                    j = i;
+                    while (j < size - 1 && prices[j] < prices[j + 1]) {
+                        ++j;
+                    }
+                    profit += prices[j] - prices[i];
+                    i = j - 1;
+                }
+                ++i;
+            }
+        }
+        return profit;
     }
 };
 
 int main() {
-    vector<int> price = {7, 1, 5, 3, 6, 4};
+    vector<int> prices = {1, 2, 3, 4, 5};
 
     Solution solution = Solution();
-    int maxProfit = solution.maxProfit(price);
+    int maxProfit = solution.maxProfit(prices);
 
     std::cout << maxProfit << std::endl;
 
