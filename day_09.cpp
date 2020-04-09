b1f69b5647d45dbaeb64ad5720e2af4307af8428
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-04-09 17:31:52 +0300

    day 9 commit

---
+++ day_09.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::vector;
+
+class Solution {
+public:
+    bool backspaceCompare(string S, string T) {
+
+    }
+};
+
+int main() {
+    Solution solution = Solution();
+    string S = "ab#c";
+    string T = "ad#c";
+
+    std::cout << solution.backspaceCompare(S, T) << std::endl;
+
+    return 0;
+}
