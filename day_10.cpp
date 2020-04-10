0fe7cfe869b79351122684a3d578dd9b102d30d3
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-04-10 10:22:40 +0300

    day 10 commit

--- day_10.cpp
+++ day_10.cpp
@@ -15,30 +15,33 @@ public:
     }
 
     void push(int x) {
-
+        minStack.push_back(x);
     }
 
     void pop() {
-
+        minStack.pop_back();
     }
 
     int top() {
-
+        return minStack.back();
     }
 
     int getMin() {
-
+        return *std::min_element(minStack.begin(), minStack.end());
     }
+private:
+    vector<int> minStack;
 };
 
 int main() {
-    int x = 10;
-    //Your MinStack object will be instantiated and called as such:
-    MinStack *obj = new MinStack();
-    obj->push(x);
-    obj->pop();
-    int param_3 = obj->top();
-    int param_4 = obj->getMin();
+    auto *minStack = new MinStack();
+    minStack->push(-2);
+    minStack->push(0);
+    minStack->push(-3);
+    std::cout << minStack->getMin() << std::endl;   //--> Returns -3.
+    minStack->pop();
+    std::cout << minStack->top()  << std::endl;    // --> Returns 0.
+    std::cout << minStack->getMin()  << std::endl;  // --> Returns -2.
 
     return 0;
 }
