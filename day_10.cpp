79bcab892fb027a3ac36e5348305fd2e0b9a4740
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-04-10 10:09:26 +0300

    day 10 commit

---
+++ day_10.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <iostream>
+#include <numeric>
+#include <map>
+
+using std::vector;
+using std::string;
+using std::map;
+
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+
+    }
+
+    void push(int x) {
+
+    }
+
+    void pop() {
+
+    }
+
+    int top() {
+
+    }
+
+    int getMin() {
+
+    }
+};
+
+int main() {
+    int x = 10;
+    //Your MinStack object will be instantiated and called as such:
+    MinStack *obj = new MinStack();
+    obj->push(x);
+    obj->pop();
+    int param_3 = obj->top();
+    int param_4 = obj->getMin();
+
+    return 0;
+}
