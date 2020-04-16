adb5edef80c49699036b36713d48f000680b0a61
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-04-16 10:37:44 +0300

    day 16 commit

---
+++ day_16.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using std::vector;
+using std::string;
+
+class Solution {
+public:
+    bool checkValidString(string s) {
+
+    }
+};
+
+int main() {
+    Solution solution = Solution();
+    string str = "(*)";
+
+    std::cout << solution.checkValidString(str) << std::endl;
+
+    return 0;
+}
