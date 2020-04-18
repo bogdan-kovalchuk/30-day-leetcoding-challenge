a2160bcffdd6061fc8836d0c4b3648e14a8aee5f
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-04-18 14:14:19 +0300

    day 18 commit

--- day_18.cpp
+++ day_18.cpp
@@ -9,7 +9,22 @@ using std::string;
 class Solution {
 public:
     int minPathSum(vector<vector<int>> &grid) {
+        if (grid.empty()) return 0;
+        int min = INT_MAX;
+        cumulate_sum(grid, 0, 0, 0, min);
+        return min;
+    }
 
+    void cumulate_sum(const vector<vector<int>> &grid, int i, int j, int sum, int &min) {
+        if (i < grid.size() - 1) {
+            cumulate_sum(grid, i + 1, j, sum + grid[i][j], min);
+        }
+        if (j < grid[0].size() - 1) {
+            cumulate_sum(grid, i, j + 1, sum + grid[i][j], min);
+        }
+        if (i == grid.size() - 1 && j == grid[0].size() - 1 && sum + grid[i][j] < min) {
+            min = sum + grid[i][j];
+        }
     }
 };
 
