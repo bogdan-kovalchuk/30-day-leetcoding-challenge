4868598fd0c26ddc9b3297a18ad376d59569205e
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-04-20 14:10:53 +0300

    day 20 commit

--- day_20.cpp
+++ day_20.cpp
@@ -18,7 +18,24 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode *bstFromPreorder(vector<int> &preorder) {
+        int index = 0;
 
+        return createNode(preorder, index, INT_MIN, INT_MAX);
+    }
+
+    TreeNode *createNode(vector<int> &preorder, int &index, int min, int max){
+        if (index == preorder.size()) return nullptr;
+
+        int number = preorder[index];
+        if(number < min || number > max) return nullptr;
+
+        auto *root = new TreeNode(number);
+        index++;
+
+        root->left = createNode(preorder, index, min, number - 1);
+        root->right = createNode(preorder, index, number + 1, max);
+
+        return root;
     }
 };
 
@@ -27,7 +44,7 @@ int main() {
 
     vector<int> preorder = {8, 5, 1, 7, 10, 12};
 
-    TreeNode *node = solution.bstFromPreorder(preorder);
+    TreeNode *root = solution.bstFromPreorder(preorder);
 
     return 0;
 }
