c4b2fb8eb9e12cb27fe512f439fe4b118c3b4d75
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-04-21 22:06:36 +0300

    day 21 commit

--- day_21.cpp
+++ day_21.cpp
@@ -10,24 +10,62 @@ using std::string;
 // You should not implement it, or speculate about its implementation
 class BinaryMatrix {
 public:
-    int get(int x, int y);
+    int get(int x, int y) {
+        return mat[x][y];
+    }
 
-    vector<int> dimensions();
+    vector<int> dimensions() {
+        vector<int> out;
+        out.emplace_back(mat.size());
+        out.emplace_back(mat[0].size());
+        return out;
+    }
+
+private:
+    vector<vector<int>> mat = {{0, 0, 0, 1, 1, 1, 1, 1, 1, 1},
+                               {0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
+                               {0, 0, 0, 0, 0, 1, 1, 1, 1, 1},
+                               {0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                               {0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+                               {0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
+                               {0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+                               {0, 0, 0, 0, 0, 1, 1, 1, 1, 1},
+                               {0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+                               {0, 0, 0, 0, 0, 1, 1, 1, 1, 1}};
 };
 
 class Solution {
 public:
     int leftMostColumnWithOne(BinaryMatrix &binaryMatrix) {
+        vector<int> dim = binaryMatrix.dimensions();
+        int i = 0, j = dim[1] - 1, num = 0;
 
+        for (; i < dim[0]; ++i) {
+            num = binaryMatrix.get(i, j);
+            if (num == 1) {
+                --j;
+                break;
+            }
+        }
+
+        if (i == dim[0]) return -1;
+
+        while (j >= 0 && i < dim[0]) {
+            num = binaryMatrix.get(i, j);
+            if (num == 0) { ++i; }
+            if (num == 1) { --j; }
+        }
+
+        return j + 1;
     }
 };
 
 int main() {
     Solution solution = Solution();
 
-    BinaryMatrix preorder;
+    BinaryMatrix mat;
 
-    std::cout << solution.leftMostColumnWithOne(preorder) << std::endl;
+    std::cout << solution.leftMostColumnWithOne(mat) << std::endl;
 
     return 0;
 }
