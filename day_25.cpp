d2a87509348e61bd156d30b822cc3a8b66958496
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-04-25 13:19:03 +0300

    day 25 commit

--- day_25.cpp
+++ day_25.cpp
@@ -9,14 +9,22 @@ using std::string;
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        int max_jump = 0;
+        for (const auto &num : nums){
+            num >= max_jump - 1 ? max_jump = num : --max_jump;
 
+            if(num == 0 && max_jump == 0 && &nums.back() != &num){
+                return false;
+            }
+        }
+        return true;
     }
 };
 
 int main() {
     Solution solution = Solution();
 
-    vector<int> nums = {2,3,1,1,4};
+    vector<int> nums = {2,3,1,1,1,0};
 
     std::cout << solution.canJump(nums) << std::endl;
 
