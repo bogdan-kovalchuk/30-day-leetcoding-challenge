52ba7879d5e59645fd747e5c825de68d6a635435
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-04-25 11:29:41 +0300

    day 25 commit

---
+++ day_25.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <iostream>
+#include <numeric>
+#include <string>
+
+using std::vector;
+using std::string;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+
+    }
+};
+
+int main() {
+    Solution solution = Solution();
+
+    vector<int> nums = {2,3,1,1,4};
+
+    std::cout << solution.canJump(nums) << std::endl;
+
+    return 0;
+}
