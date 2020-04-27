266a6e52d4a4ba3a81916510e8b4895a834e6482
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-04-27 10:41:35 +0300

    day 27 commit

---
+++ day_27.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <iostream>
+#include <numeric>
+#include <string>
+
+using std::vector;
+using std::string;
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+
+    }
+};
+
+int main() {
+    Solution solution = Solution();
+
+    vector<vector<char>> matrix;
+
+    std::cout << solution.maximalSquare(matrix) << std::endl;
+
+    return 0;
+}
