90793542d9e2d9bab5f3c9c1060c9b53b426d876
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2020-09-02 14:00:00 +0300

    add sort-based single number solution

--- day_01.cpp
+++ day_01.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <iostream>
 #include <map>
+#include <algorithm>
 
 using std::vector;
 using std::map;
@@ -37,15 +38,30 @@ public:
     }
 };
 
+class SolutionSort {
+public:
+    int singleNumber(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i + 1 < (int)nums.size(); i += 2) {
+            if (nums[i] != nums[i + 1]) return nums[i];
+        }
+        return nums.back();
+    }
+};
+
 int main() {
     Solution solution = Solution();
     SolutionXOR solutionXOR = SolutionXOR();
+    SolutionSort solutionSort = SolutionSort();
     vector<int> nums = {2, 2, 5, 8, 9, 5, 1, 1, 9};
+    vector<int> numsCopy1(nums.begin(), nums.end());
+    vector<int> numsCopy2(nums.begin(), nums.end());
 
     int out = solution.singleNumber(nums);
-    int outXOR = solutionXOR.singleNumber(nums);
+    int outXOR = solutionXOR.singleNumber(numsCopy1);
+    int outSort = solutionSort.singleNumber(numsCopy2);
 
-    std::cout << "Map: " << out << " XOR: " << outXOR << std::endl;
+    std::cout << "Map: " << out << " XOR: " << outXOR << " Sort: " << outSort << std::endl;
 
     return 0;
 }
