7dc756f59437f5803ce005bb32d53f4e64ae4a1d
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2020-09-24 10:30:00 +0300

    add one-pass and state machine solutions for stock profit

--- day_05.cpp
+++ day_05.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <numeric>
 #include <limits>
+#include <algorithm>
 
 using std::vector;
 
@@ -30,13 +31,46 @@ public:
     }
 };
 
+class SolutionOnePass {
+public:
+    int maxProfit(vector<int> &prices) {
+        int profit = 0;
+        for (size_t i = 1; i < prices.size(); ++i) {
+            if (prices[i] > prices[i - 1])
+                profit += prices[i] - prices[i - 1];
+        }
+        return profit;
+    }
+};
+
+class SolutionStateMachine {
+public:
+    int maxProfit(vector<int> &prices) {
+        if (prices.empty()) return 0;
+        int hold = -prices[0];
+        int cash = 0;
+        for (size_t i = 1; i < prices.size(); ++i) {
+            int prevCash = cash;
+            cash = std::max(cash, hold + prices[i]);
+            hold = std::max(hold, prevCash - prices[i]);
+        }
+        return cash;
+    }
+};
+
 int main() {
+    Solution solution;
+    SolutionOnePass solutionOnePass;
+    SolutionStateMachine solutionSM;
     vector<int> prices = {1, 2, 3, 4, 5};
 
-    Solution solution = Solution();
-    int maxProfit = solution.maxProfit(prices);
+    int r1 = solution.maxProfit(prices);
+    vector<int> c(prices.begin(), prices.end());
+    int r2 = solutionOnePass.maxProfit(c);
+    vector<int> c2(prices.begin(), prices.end());
+    int r3 = solutionSM.maxProfit(c2);
 
-    std::cout << maxProfit << std::endl;
+    std::cout << "ValleyPeak: " << r1 << " OnePass: " << r2 << " StateMachine: " << r3 << std::endl;
 
     return 0;
 }
