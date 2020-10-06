bfcc05c2c7c2e083fd76273b549c5e3d4f5995ca
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2020-10-06 09:30:00 +0300

    add character-count key solution for group anagrams

--- day_06.cpp
+++ day_06.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <numeric>
 #include <map>
+#include <unordered_map>
+#include <string>
+#include <algorithm>
 
 using std::vector;
 using std::string;
@@ -24,12 +27,30 @@ public:
     }
 };
 
+class SolutionCountKey {
+public:
+    vector<vector<string>> groupAnagrams(vector<string> &strs) {
+        std::unordered_map<string, vector<string>> grouped;
+        for (const auto &str : strs) {
+            string key(26, '0');
+            for (char c : str) key[c - 'a']++;
+            grouped[key].push_back(str);
+        }
+        vector<vector<string>> result;
+        for (const auto &item : grouped) result.push_back(item.second);
+        return result;
+    }
+};
+
 int main() {
     vector<string> array_of_strings = {"eat", "tea", "tan", "ate", "nat", "bat"};
 
+    Solution solution;
+    SolutionCountKey solutionCount;
+    auto r1 = solution.groupAnagrams(array_of_strings);
+    auto r2 = solutionCount.groupAnagrams(array_of_strings);
 
-    Solution solution = Solution();
-    vector<vector<string>> maxProfit = solution.groupAnagrams(array_of_strings);
+    std::cout << "SortKey groups: " << r1.size() << " CountKey groups: " << r2.size() << std::endl;
 
     return 0;
 }
