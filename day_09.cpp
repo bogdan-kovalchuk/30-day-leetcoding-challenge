75a9b2930fe3638221b56ec5f5892574fcb6b5ea
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2021-03-21 14:00:00 +0200

    add reverse two-pointer solution for backspace compare

--- day_09.cpp
+++ day_09.cpp
@@ -58,9 +58,38 @@ private:
     }
 };
 
+// Reverse two-pointer: walk both strings from the end,
+// counting '#' skips. Compare surviving characters in-place.
+// Time: O(m + n). Space: O(1).
+class Solution3 {
+public:
+    bool backspaceCompare(string S, string T) {
+        int i = static_cast<int>(S.size()) - 1;
+        int j = static_cast<int>(T.size()) - 1;
+        while (i >= 0 || j >= 0) {
+            int skipS = 0;
+            while (i >= 0 && (S[i] == '#' || skipS > 0)) {
+                if (S[i] == '#') { ++skipS; --i; }
+                else { --skipS; --i; }
+            }
+            int skipT = 0;
+            while (j >= 0 && (T[j] == '#' || skipT > 0)) {
+                if (T[j] == '#') { ++skipT; --j; }
+                else { --skipT; --j; }
+            }
+            if (i >= 0 && j >= 0 && S[i] != T[j]) return false;
+            if ((i >= 0) != (j >= 0)) return false;
+            --i;
+            --j;
+        }
+        return true;
+    }
+};
+
 int main() {
     Solution s1;
     Solution2 s2;
+    Solution3 s3;
 
     struct Case { string a, b; bool expected; };
     vector<Case> cases = {
@@ -82,12 +111,14 @@ int main() {
     for (const auto &tc : cases) {
         string a1 = tc.a, b1 = tc.b;
         string a2 = tc.a, b2 = tc.b;
+        string a3 = tc.a, b3 = tc.b;
         bool r1 = s1.backspaceCompare(a1, b1);
         bool r2 = s2.backspaceCompare(a2, b2);
-        bool ok = (r1 == r2) && (r2 == tc.expected);
+        bool r3 = s3.backspaceCompare(a3, b3);
+        bool ok = (r1 == r2) && (r2 == r3) && (r3 == tc.expected);
         if (!ok) allOk = false;
         std::cout << "\"" << tc.a << "\" vs \"" << tc.b << "\": "
-                  << "erase=" << r1 << " stack=" << r2
+                  << "erase=" << r1 << " stack=" << r2 << " ptr=" << r3
                   << " expected=" << tc.expected
                   << (ok ? " OK" : " FAIL") << std::endl;
     }
@@ -97,6 +128,7 @@ int main() {
 }
 
 // Complexity comparison:
-// Solution  (in-place erase): Time O(n^2) worst case (erase shifts), Space O(1).
-// Solution2 (stack buffer):   Time O(n),               Space O(n).
-// Stack approach trades memory for linear time.
+// Solution  (in-place erase):      Time O(n^2) worst case, Space O(1).
+// Solution2 (stack buffer):        Time O(n),               Space O(n).
+// Solution3 (reverse two-pointer): Time O(n),               Space O(1).
+// Reverse-pointer gives linear time with constant extra space.
