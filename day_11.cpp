547040c1a0be798f609859bb8b1417db0e7196c9
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2021-04-17 10:00:00 +0300

    add BFS-based diameter of binary tree solution

--- day_11.cpp
+++ day_11.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <map>
 #include <utility>
+#include <queue>
+#include <unordered_map>
+#include <algorithm>
 
 using std::vector;
 using std::map;
@@ -60,26 +63,81 @@ public:
     }
 };
 
-int main() {
-    Solution solution = Solution();
-    map<int, pair<int, int>> tree = {{1, {2, 3}},
-                                     {2, {4, 5}},
-                                     {5, {6, 7}},};
-
-    auto *root = new TreeNode(tree.begin()->first);
-    for (const auto &elem: tree) {
-        auto *target_root = TreeNode::findNode(root, elem.first);
-        if (elem.second.first != 0) {
-            target_root->left = new TreeNode(elem.second.first);
+// BFS-based: collect nodes level-by-level, then process bottom-up.
+// For each node compute depth from children; diameter = max(left+right).
+// Time: O(n). Space: O(n) for queue + depth map.
+class Solution2 {
+public:
+    int diameterOfBinaryTree(TreeNode *root) {
+        if (!root) return 0;
+        std::vector<TreeNode*> order;
+        std::queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            TreeNode *node = q.front(); q.pop();
+            order.push_back(node);
+            if (node->left)  q.push(node->left);
+            if (node->right) q.push(node->right);
         }
-        if (elem.second.second != 0) {
-            target_root->right = new TreeNode(elem.second.second);
+        std::unordered_map<TreeNode*, int> depth;
+        int diameter = 0;
+        for (int i = static_cast<int>(order.size()) - 1; i >= 0; --i) {
+            TreeNode *node = order[i];
+            int ld = node->left  ? depth[node->left]  + 1 : 0;
+            int rd = node->right ? depth[node->right] + 1 : 0;
+            depth[node] = std::max(ld, rd);
+            diameter = std::max(diameter, ld + rd);
         }
+        return diameter;
     }
+};
 
-    int diameter = solution.diameterOfBinaryTree(root);
+static TreeNode *buildTree(const map<int, pair<int,int>> &spec) {
+    auto *root = new TreeNode(spec.begin()->first);
+    for (const auto &elem : spec) {
+        auto *target = TreeNode::findNode(root, elem.first);
+        if (elem.second.first != 0)
+            target->left = new TreeNode(elem.second.first);
+        if (elem.second.second != 0)
+            target->right = new TreeNode(elem.second.second);
+    }
+    return root;
+}
 
-    std::cout << diameter << std::endl;
+static void freeTree(TreeNode *node) {
+    if (!node) return;
+    freeTree(node->left);
+    freeTree(node->right);
+    delete node;
+}
+
+int main() {
+    Solution s1;
+    Solution2 s2;
+
+    vector<map<int, pair<int,int>>> trees = {
+        {{1, {2, 3}}, {2, {4, 5}}},
+        {{1, {2, 0}}},
+        {{1, {2, 3}}, {2, {4, 5}}, {5, {6, 7}}},
+        {{1, {0, 0}}},
+        {{1, {2, 3}}, {3, {4, 0}}},
+    };
+
+    for (const auto &spec : trees) {
+        auto *r1 = buildTree(spec);
+        auto *r2 = buildTree(spec);
+        int d1 = s1.diameterOfBinaryTree(r1);
+        int d2 = s2.diameterOfBinaryTree(r2);
+        std::cout << "dfs=" << d1 << " bfs=" << d2
+                  << (d1 == d2 ? " OK" : " MISMATCH") << std::endl;
+        freeTree(r1);
+        freeTree(r2);
+    }
 
     return 0;
 }
+
+// Complexity comparison:
+// Solution  (recursive DFS):         Time O(n), Space O(h) call-stack.
+// Solution2 (BFS + bottom-up map):   Time O(n), Space O(n) queue + map.
+// Both linear; DFS uses less memory on balanced trees (O(log n) stack vs O(n) map).
