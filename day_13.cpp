04f2b77b471336066905acdf230ab1aaf728653e
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2021-05-21 10:00:00 +0300

    add brute force solution for contiguous array

--- day_13.cpp
+++ day_13.cpp
@@ -24,13 +24,51 @@ public:
     }
 };
 
+// Brute force: check every subarray, count 0s and 1s.
+// Time: O(n^2). Space: O(1).
+class Solution2 {
+public:
+    int findMaxLength(vector<int> &nums) {
+        int best = 0;
+        for (int i = 0; i < static_cast<int>(nums.size()); ++i) {
+            int zeros = 0, ones = 0;
+            for (int j = i; j < static_cast<int>(nums.size()); ++j) {
+                if (nums[j] == 0) ++zeros; else ++ones;
+                if (zeros == ones) best = std::max(best, j - i + 1);
+            }
+        }
+        return best;
+    }
+};
+
 int main() {
-    Solution solution = Solution();
-    vector<int> nums = {0, 1, 1};
+    Solution s1;
+    Solution2 s2;
 
-    int max_length = solution.findMaxLength(nums);
+    vector<vector<int>> cases = {
+        {0, 1},
+        {0, 1, 0},
+        {0, 1, 1},
+        {0, 0, 1, 1, 0, 1, 0, 0},
+        {1, 1, 1, 1},
+        {0, 0, 0, 0},
+        {},
+        {0, 1, 0, 1, 0, 1},
+        {1, 0, 0, 1, 1, 0},
+    };
 
-    std::cout << max_length << std::endl;
+    for (const auto &nums : cases) {
+        auto v1 = nums, v2 = nums;
+        int r1 = s1.findMaxLength(v1);
+        int r2 = s2.findMaxLength(v2);
+        std::cout << "map=" << r1 << " brute=" << r2
+                  << (r1 == r2 ? " OK" : " MISMATCH") << std::endl;
+    }
 
     return 0;
 }
+
+// Complexity comparison:
+// Solution  (prefix sum + hash map): Time O(n), Space O(n).
+// Solution2 (brute force):           Time O(n^2), Space O(1).
+// Hash-map approach is optimal; brute force useful as reference for small inputs.
