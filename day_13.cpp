27102f492f78d50fe7289cba74e11edccffc01a6
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2021-05-21 14:00:00 +0300

    add vector-offset prefix sum solution for contiguous array

--- day_13.cpp
+++ day_13.cpp
@@ -41,9 +41,34 @@ public:
     }
 };
 
+// Prefix sum + vector offset: sum ranges from -n to +n,
+// so use vector of size 2n+1 with offset n. O(1) lookups vs hash map.
+// Time: O(n). Space: O(n).
+class Solution3 {
+public:
+    int findMaxLength(vector<int> &nums) {
+        int n = static_cast<int>(nums.size());
+        vector<int> first_seen(2 * n + 1, -2);
+        int offset = n;
+        first_seen[0 + offset] = -1;
+        int sum = 0, best = 0;
+        for (int i = 0; i < n; ++i) {
+            sum += (nums[i] == 1 ? 1 : -1);
+            int idx = sum + offset;
+            if (first_seen[idx] >= -1) {
+                best = std::max(best, i - first_seen[idx]);
+            } else {
+                first_seen[idx] = i;
+            }
+        }
+        return best;
+    }
+};
+
 int main() {
     Solution s1;
     Solution2 s2;
+    Solution3 s3;
 
     vector<vector<int>> cases = {
         {0, 1},
@@ -58,17 +83,19 @@ int main() {
     };
 
     for (const auto &nums : cases) {
-        auto v1 = nums, v2 = nums;
+        auto v1 = nums, v2 = nums, v3 = nums;
         int r1 = s1.findMaxLength(v1);
         int r2 = s2.findMaxLength(v2);
-        std::cout << "map=" << r1 << " brute=" << r2
-                  << (r1 == r2 ? " OK" : " MISMATCH") << std::endl;
+        int r3 = s3.findMaxLength(v3);
+        std::cout << "map=" << r1 << " brute=" << r2 << " vec=" << r3
+                  << ((r1 == r2 && r2 == r3) ? " OK" : " MISMATCH") << std::endl;
     }
 
     return 0;
 }
 
 // Complexity comparison:
-// Solution  (prefix sum + hash map): Time O(n), Space O(n).
-// Solution2 (brute force):           Time O(n^2), Space O(1).
-// Hash-map approach is optimal; brute force useful as reference for small inputs.
+// Solution  (prefix sum + hash map):  Time O(n) avg, Space O(n).
+// Solution2 (brute force):            Time O(n^2),     Space O(1).
+// Solution3 (prefix sum + vec offset): Time O(n),       Space O(n).
+// Vector offset avoids hash collisions and gives predictable O(1) lookups.
